218413dde17ab9d00fffc8f78e6d58a32e3b6346
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-10-09 18:37:28 -0400

    boiler plate for Search component using AsyncPaginate

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import Search from "./components/search/Search";
 import "./App.css";
 
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
   return (
     <div className="container">
-      <Search/>
+      <Search onSearchChange={handleOnSearchChange}/>
     </div>
   );
 }
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,22 @@
-export default function Search () {
-    return (
-        <>
-       <h1> 'Hello'</h1>
-        </>
-    )
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+export default function Search({onSearchChange}) {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  }
+
+  return (
+    <>
+      <AsyncPaginate
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+      />
+    </>
+  );
 }

8202fc861b086e2c08f0517a31c4b9627ab20cfe
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-10-09 20:39:49 -0400

    built current weather component that fetches data by city from api to display current weather

--- src/components/currentWeather/CurrentWeather.jsx
+++ src/components/currentWeather/CurrentWeather.jsx
@@ -1,40 +1,42 @@
 import "./currentWeather.css";
 
-export default function CurrentWeather() {
+export default function CurrentWeather({ data }) {
   return (
     <div className="weather">
       <div className="top">
         <div>
-          <p className="city">Belgium</p>
-          <p className="weather-description">Cloudy</p>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
         </div>
         <img
           className="weather-icon"
-          src="../weatherIcons/04d.png"
+          src={`../weatherIcons/${data.weather[0].icon}.png`}
           alt="weather"
         />
       </div>
       <div className="bottom">
-        <p className="temperature">18°C</p>
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
         <div className="details">
           <div className="parameter-row">
             <span className="parameter-label">Details</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">22°C</span>
+            <span className="parameter-value">
+              {Math.round(data.main.feels_like)}°C
+            </span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
-            <span className="parameter-value">30%</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Pressure</span>
-            <span className="parameter-value">15 h</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
           </div>
         </div>
       </div>

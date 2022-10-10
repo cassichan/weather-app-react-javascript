8d07b03f4e51cd80992c92b5a2bc012b2ccb2359
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-10-09 21:27:35 -0400

    added 7 day forecast component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Search from "./components/search/Search";
 import CurrentWeather from "./components/currentWeather/CurrentWeather";
+import Forecast from "./components/forecast/Forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 import { useState } from "react";
@@ -39,6 +40,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
---
+++ src/components/forecast/Forecast.jsx
@@ -0,0 +1,54 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemButton,
+  AccordionItemHeading,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export default function Forecast({ data }) {
+  //Determine day in a week, returns a number
+  const dayInWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInWeek)
+  );
+  // console.log(forecastDays)
+
+  return (
+    <>
+      <label className="title">Daily</label>
+      {/* Show 7 days of data */}
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, index) => (
+          <AccordionItem key={index}>
+            <AccordionItemHeading>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    src={`weatherIcons/${item.weather[0].icon}.png`}
+                    alt="weather"
+                    className="icon-small"
+                  />
+                  <label className="day">{forecastDays[index]}</label>
+                  <label className="description">{item.weather[0].description}</label>
+                  <label className="min-max">{Math.round(item.main.temp.temp_min)}°C / {Math.round(item.main.temp.temp_max)}°C</label>
+                </div>
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+}
